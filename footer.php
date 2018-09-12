1b93b220ad6480f3b76cfdc480c563308af3cdea
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2018-09-12 11:38:43 +0300

    delete all

--- footer.php
+++
@@ -1,6 +0,0 @@
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDrhyfhXZe_t_nQBp44C9__IRfv5Q1j5cE"
-        type="text/javascript"></script>
-<script type="text/javascript" src="./site/assets/js/vendor.js"></script>
-<script type="text/javascript" src="./site/assets/js/app.js"></script>
-</body>
-</html>
